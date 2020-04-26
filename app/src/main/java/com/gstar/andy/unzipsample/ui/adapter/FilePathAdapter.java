dfe7c3f6915aad8a9356145fad62e247ee1a3248
renjie <renjiebaidu@163.com>
2020-04-26 14:51:50 +0800

    File Update

--- app/src/main/java/com/gstar/andy/unzipsample/ui/adapter/FilePathAdapter.java
+++ app/src/main/java/com/gstar/andy/unzipsample/ui/adapter/FilePathAdapter.java
@@ -1,6 +1,6 @@
 package com.gstar.andy.unzipsample.ui.adapter;
 
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
