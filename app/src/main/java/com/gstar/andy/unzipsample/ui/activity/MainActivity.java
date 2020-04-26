79de6075aee6520db75f18be0383476d3dc1fc2f
renjie <renjiebaidu@163.com>
2020-04-26 14:53:53 +0800

    File Update

--- app/src/main/java/com/gstar/andy/unzipsample/ui/activity/MainActivity.java
+++ app/src/main/java/com/gstar/andy/unzipsample/ui/activity/MainActivity.java
@@ -2,12 +2,13 @@ package com.gstar.andy.unzipsample.ui.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
